1fa1e5321eca976621f006dcef05f7ba10408556
MarcusAfolabi <biormarcus@gmail.com>
2023-05-17 14:38:09 +0100

    user updated

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -3,10 +3,14 @@
 namespace App\Actions\Fortify;
 
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
-use Laravel\Fortify\Contracts\CreatesNewUsers;
+use App\Mail\WelcomeMail;
 use Laravel\Jetstream\Jetstream;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use App\Notifications\AdminNotification;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Notifications\Notification;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
 
 class CreateNewUser implements CreatesNewUsers
 {
@@ -17,6 +21,21 @@ class CreateNewUser implements CreatesNewUsers
      *
      * @param  array<string, string>  $input
      */
+    // public function create(array $input): User
+    // {
+    //     Validator::make($input, [
+    //         'name' => ['required', 'string', 'max:255'],
+    //         'email' => ['required', 'string', 'email', 'rfc', 'dns', 'max:255', 'unique:users'],
+    //         'password' => $this->passwordRules(),
+    //         'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
+    //     ])->validate();
+
+    //     return User::create([
+    //         'name' => $input['name'],
+    //         'email' => $input['email'],
+    //         'password' => Hash::make($input['password']),
+    //     ]);
+    // }
     public function create(array $input): User
     {
         Validator::make($input, [
@@ -26,10 +45,20 @@ class CreateNewUser implements CreatesNewUsers
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
-        return User::create([
+        $user = User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
+
+        // Send welcome email to the newly registered user
+        Mail::to($user->email)->send(new WelcomeMail($user));
+
+        // Send notification to the admin email
+        Notification::route('mail', [
+            'info@metrip.com' => 'Alert! New User registerd',
+        ])->notify(new AdminNotification($user));
+
+        return $user;
     }
 }
